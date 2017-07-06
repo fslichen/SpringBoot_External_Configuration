e3046ea9e73cdbb1acbf79a654597c6b2c9aff90
fslic <fslic@DESKTOP-MCATCHJ>
2017-07-06 23:28:36 +0800

    initial commit

---
+++ SpringBoot_External_Configuration/src/main/resources/shell.sh
@@ -0,0 +1,11 @@
+# Run the naked project.
+java -jar run.war
+
+# Run the project on port 8081.
+java -jar -Dserver.port=8081 run.war
+
+# Run the project by application-dev.properties on classpath.
+java -jar -Dspring.config.location=classpath:/application-dev.properties run.war
+
+# Run the project by application-dev.properties under config folder in classpath.
+java -jar -Dspring.config.location=classpath:/config/application-pro.properties run.war
