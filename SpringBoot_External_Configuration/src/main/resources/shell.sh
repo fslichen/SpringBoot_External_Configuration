62cf6558bfc489004bdcee12977f3e25333fdab5
fslic <fslic@DESKTOP-MCATCHJ>
2017-07-06 23:35:07 +0800

    refactor

--- SpringBoot_External_Configuration/src/main/resources/shell.sh
+++ SpringBoot_External_Configuration/src/main/resources/shell.sh
@@ -9,3 +9,6 @@ java -jar -Dspring.config.location=classpath:/application-dev.properties run.war
 
 # Run the project by application-dev.properties under config folder in classpath.
 java -jar -Dspring.config.location=classpath:/config/application-pro.properties run.war
+
+# Run the project by application-test.properties under file path.
+java -jar -Dspring.config.location=file:/D:/Buffer/application-test.properties run.war
